62a5831cde97f0c66b64e820d374cd2d51a31ab5
Eliza Romeikat <eliza-romeikat@gmx.de>
2023-08-25 15:49:38 +0200

    create headline for list component

--- src/components/list.js
+++ src/components/list.js
@@ -1,6 +1,11 @@
-export default function List({ activities }) {
+export default function List({ activities, isGoodWeather }) {
   return (
     <>
+      <h1>
+        {isGoodWeather
+          ? "The weather is awesome, go outside and:"
+          : "Bad weather outside! Here is what we can do:"}
+      </h1>
       <ul>
         {activities.map((activity) => {
           return <li key={activity.id}>{activity.name}</li>;
