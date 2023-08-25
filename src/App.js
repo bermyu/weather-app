1f074190aedf5a23a746be91ef19038ebc964298
Eliza Romeikat <eliza-romeikat@gmx.de>
2023-08-25 14:08:18 +0200

    add state and handle function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import Form from "./components/form.js";
+import { uid } from "uid";
 
 function App() {
+  const [activities, setActivities] = useState([]);
+  function handleAddActivity(newActivity) {
+    setActivities([...activities, { ...newActivity, id: uid() }]);
+  }
   return (
     <div className="App">
-      <Form />
+      <Form onAddActivity={handleAddActivity} />
     </div>
   );
 }
